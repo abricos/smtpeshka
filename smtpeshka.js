e0ce4b30c031875a157e6eaefbb755d1eda42678
Alexander Kuzmin <roosit@abricos.org>
2015-01-20 17:35:39 +0300

    draft: starting web and smtp servers

--- smtpeshka.js
+++ smtpeshka.js
@@ -7,16 +7,19 @@ var HarakaConfig = require('./lib/HarakaConfig');
 var harakaConfig = new HarakaConfig();
 
 harakaConfig.build(function(err, harakaConfigDir){
-    console.log(harakaConfigDir);
 
     var base = path.join(__dirname);
+    var webServer = path.join(base, 'lib/web/index.js');
+
+    // start web server
+    require(webServer);
+
     var harakaPath = path.join(base, 'node_modules/Haraka/haraka.js');
 
     process.argv[1] = harakaPath;
     process.env.HARAKA = harakaConfigDir;
-    require(harakaPath);
-});
 
-process.on('exit', function(code){
-    console.log('exit code: ' + code);
+    //start SMTP Hakara server
+    require(harakaPath);
+
 });
