5a9426e05a41794aee27e3911cf67d22e4009c41
Alexander Kuzmin <roosit@abricos.org>
2015-01-21 16:09:19 +0300

    console.log to Tree-config.logger

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var cover = require('gulp-coverage');
+var jsdoc = require('gulp-jsdoc');
+var subtree = require('gulp-subtree');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
 
 var opts = {
     mocha: {
@@ -20,10 +25,51 @@ gulp.task('test', function(){
 });
 
 gulp.task('watch', function(){
-    var watcher = gulp.watch(['index.js', 'lib/**', 'test/**'], ['test']);
+    var watcher = gulp.watch(['lib/**', 'test/**'], ['test']);
     watcher.on('change', function(event){
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 });
+
+function coverage(tests, output){
+    return gulp
+        .src(tests, {read: false})
+        .pipe(cover.instrument({
+            pattern: ['index.js', 'lib/**.js'],
+            debugDirectory: 'debug'
+        }))
+        .pipe(mocha(opts.mocha))
+        .pipe(cover.report({
+            outFile: output
+        }));
+}
+
+gulp.task('coverage', function(){
+    return coverage(['test/*.js'], 'test/coverage.html');
+});
+
+gulp.task('clean-docs', function(){
+    return gulp.src(['docs/'])
+        .pipe(vinylPaths(del));
+});
+
+gulp.task('make-docs', function(){
+    return gulp.src(['index.js', 'lib/**/*.js', 'README.md'])
+        .pipe(jsdoc('./docs'));
+});
+
+/*
+gulp.task('publish-docs', function(){
+    return gulp
+        .src('docs')
+        .pipe(subtree({
+            remote: 'origin',
+            branch: 'master',
+            message: 'Updating docs'
+        }));
+});
+/**/
+
+gulp.task('docs', ['clean-docs', 'make-docs']);
 
 gulp.task('default', ['test', 'watch']);
